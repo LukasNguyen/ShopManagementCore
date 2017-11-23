99fb2bfd032201f3ab7ee24779ba9d7bb977cd48
Lukas Nguyen <Dat.NguyenThaiThanh@studentpartner.com>
2017-11-23 16:03:45 +0700

    Seeding data and config startup web

--- SaleShopCore/SaleShopCore/Startup.cs
+++ SaleShopCore/SaleShopCore/Startup.cs
@@ -9,6 +9,8 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using SaleShopCore.Data;
+using SaleShopCore.Data.EF;
+using SaleShopCore.Data.Entities;
 using SaleShopCore.Models;
 using SaleShopCore.Services;
 
@@ -26,21 +28,24 @@ namespace SaleShopCore
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddDbContext<ApplicationDbContext>(options =>
-                options.UseSqlServer(Configuration.GetConnectionString("DefaultConnection")));
+            services.AddDbContext<AppDbContext>(options =>
+                options.UseSqlServer(Configuration.GetConnectionString("DefaultConnection"),n=>n.MigrationsAssembly("SaleShopCore.Data.EF")));
 
-            services.AddIdentity<ApplicationUser, IdentityRole>()
-                .AddEntityFrameworkStores<ApplicationDbContext>()
+            services.AddIdentity<AppUser, AppRole>()
+                .AddEntityFrameworkStores<AppDbContext>()
                 .AddDefaultTokenProviders();
 
             // Add application services.
+            services.AddScoped<UserManager<AppUser>, UserManager<AppUser>>();
+            services.AddScoped<RoleManager<AppRole>, RoleManager<AppRole>>();
             services.AddTransient<IEmailSender, EmailSender>();
+            services.AddTransient<DbIntializer>();
 
             services.AddMvc();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env,DbIntializer dbIntializer)
         {
             if (env.IsDevelopment())
             {
@@ -63,6 +68,8 @@ namespace SaleShopCore
                     name: "default",
                     template: "{controller=Home}/{action=Index}/{id?}");
             });
+
+            dbIntializer.Seed().Wait();
         }
     }
 }
