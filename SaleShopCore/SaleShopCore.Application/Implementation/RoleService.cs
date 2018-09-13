053058d30bc41f6dec6f4eab5d72e752695c19f3
Lukas Nguyen <datnguyen.msp@outlook.com>
2018-09-13 21:27:26 +0700

    Implemented role management feature.

--- SaleShopCore/SaleShopCore.Application/Implementation/RoleService.cs
+++ SaleShopCore/SaleShopCore.Application/Implementation/RoleService.cs
@@ -138,7 +138,7 @@ namespace SaleShopCore.Application.Implementation
 
         public async Task UpdateAsync(AppRoleViewModel roleVm)
         {
-            var role = await _roleManager.FindByIdAsync(roleVm.Id);
+            var role = await _roleManager.FindByIdAsync(roleVm.Id.ToString());
             role.Description = roleVm.Description;
             role.Name = roleVm.Name;
             await _roleManager.UpdateAsync(role);
--- SaleShopCore/SaleShopCore.Application/ViewModels/System/AppRoleViewModel.cs
+++ SaleShopCore/SaleShopCore.Application/ViewModels/System/AppRoleViewModel.cs
@@ -4,7 +4,7 @@ namespace SaleShopCore.Application.ViewModels.System
 {
     public class AppRoleViewModel
     {
-        public string Id { set; get; }
+        public Guid? Id { set; get; }
 
         public string Name { set; get; }
 
